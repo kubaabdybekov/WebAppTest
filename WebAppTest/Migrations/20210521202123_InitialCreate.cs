06717a22781b01cc888603a1b88b4bdd920fe7f4
kubaabdybekov <abdybekovkuba@gmail.com>
2021-05-22 02:17:01 +0500

    Добавьте файлы проекта.

---
+++ WebAppTest/Migrations/20210521202123_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppTest.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Developer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}
---
+++ WebAppTest/Models/GameGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace WebAppTest.Models
+{
+    public class GameGenreViewModel
+    {
+        public List<Game> Games { get; set; }
+        public SelectList Genres { get; set; }
+        public string GameGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
